939540d8cc60cb62bf1eb2fbfc336d09dbfce5ce
margaiwangara <margaiwangara@gmail.com>
2020-01-19 23:20:18 +0300

    modified homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
 import app from "../base";
+import { AuthContext } from "./Auth";
 
 export default function Home() {
+  const { currentUser } = useContext(AuthContext);
   return (
-    <>
-      <h3>Homepage</h3>
-      <button onClick={() => app.auth().signOut()}>Sign Out</button>
-    </>
+    <div className="row align-items-center">
+      <div className="col">
+        <h5 className="display-5">Chats</h5>
+      </div>
+      <div className="col align-items-end">
+        <div className="dropdown">
+          <a
+            href="#"
+            className="btn btn-secondary dropdown-toggle"
+            id="dropdownMenuLink"
+            data-toggle="dropdown"
+          >
+            {currentUser.email.split("@")[0]}
+          </a>
+
+          <div className="dropdown-menu">
+            <a href="#" className="dropdown-item">
+              Profile
+            </a>
+            <a
+              href="#"
+              onClick={() => app.auth().signOut()}
+              className="dropdown-item"
+            >
+              Sign Out
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
