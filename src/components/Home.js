48ea28d2418553f76fb34529b9e6212cfa307beb
margaiwangara <margaiwangara@gmail.com>
2020-01-19 09:18:56 +0300

    Simple boilerplate to confirm working route

---
+++ src/components/Home.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Home() {
+  return <h3>This is my Homepage</h3>;
+}
---
+++ src/components/Register.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Register() {
+  return <h3>This is my Register Page</h3>;
+}
